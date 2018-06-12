188b676a185a1be084b0c9b00f008c5d450b1cd3
Jake Ryan <jake@subtextdigital.com>
2018-06-12 23:23:28 +0000

    add sample custom post type declaration function

--- web/app/themes/asterix/functions.php
+++ web/app/themes/asterix/functions.php
@@ -34,3 +34,58 @@ function load_custom_wp_admin_style() {
     wp_enqueue_style( 'custom_wp_admin_css' );
 }
 add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+
+// ********************
+// ** allow svg uploads
+// ** to wordpress
+// ********************
+//function cc_mime_types($mimes) {
+//  $mimes['svg'] = 'image/svg+xml';
+//  return $mimes;
+//}
+//add_filter('upload_mimes', 'cc_mime_types');
+
+
+// *****************
+// ** declare cpt's
+// *****************
+//function my_custom_post_types() {
+  
+  // sample data for a cpt
+  // repeat this block for declaring custom post types
+  
+  //$singular_name = 'Resource';
+  //$plural_name = 'Resources';
+  //$slug = 'resources';
+
+  //$labels = array(
+  //  'name'               => $singular_name,
+  //  'singular_name'      => $singular_name,
+  //  'menu_name'          => $plural_name,
+  //  'name_admin_bar'     => $singular_name,
+  //  'add_new'            => 'Add New',
+  //  'add_new_item'       => 'Add New ' . $singular_name,
+  //  'new_item'           => 'New ' . $singular_name,
+  //  'edit_item'          => 'Edit ' . $singular_name,
+  //  'view_item'          => 'View ' . $singular_name,
+  //  'all_items'          => 'All ' . $plural_name,
+  //  'search_items'       => 'Search ' . $plural_name,
+  //  'parent_item_colon'  => 'Parent ' . $plural_name,
+  //  'not_found'          => 'No ' . $plural_name . ' found.',
+  //  'not_found_in_trash' => 'No ' . $plural_name . ' found in Trash.'
+  //);
+
+  //$args = array( 
+  //  'public'      => true, 
+  //  'labels'      => $labels,
+  //  'has_archive' => true,
+  //  'query_var' => true,
+  //  'menu_icon' => 'dashicons-portfolio',
+  //  'menu_position' => 5,
+  //);
+  //register_post_type( $slug, $args );
+  
+  // end sample data for a cpt
+//}
+//add_action( 'init', 'skyward_custom_post_types' );
