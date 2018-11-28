c08246e43802dc9da1fc9bb8b34849d65dd03335
Jake Ryan <jake@subtextdigital.com>
2018-11-28 12:17:51 -0500

    add sample acf options page to functions.php

--- web/app/themes/asterix/functions.php
+++ web/app/themes/asterix/functions.php
@@ -134,3 +134,28 @@ add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
 // }
 // add_action( 'init', 'asterix_custom_taxonomies', 0 );
+
+// ****************************
+// ** declare ACF Options Pages
+// ****************************
+// if( function_exists('acf_add_options_page') ) {
+//   acf_add_options_page(array(
+//     'page_title'  => 'General Settings',
+//     'menu_title'  => 'Settings',
+//     'menu_slug'   => 'general-settings',
+//     'capability'  => 'edit_posts',
+//     'redirect'    => false
+//   ));
+  
+//   acf_add_options_sub_page(array(
+//     'page_title'  => 'Header Settings',
+//     'menu_title'  => 'Header',
+//     'parent_slug' => 'general-settings',
+//   ));
+  
+//   acf_add_options_sub_page(array(
+//     'page_title'  => 'Footer Settings',
+//     'menu_title'  => 'Footer',
+//     'parent_slug' => 'general-settings',
+//   ));
+// }
